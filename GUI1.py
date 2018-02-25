83b2317bbc71ebc2e07f1501f33cfed9aee055b7
federicogottardo <federicogottard@gmail.com>
2018-02-25 20:09:22 +0000

    change

--- GUI1.py
+++ GUI1.py
@@ -1,3 +1,19 @@
 from tkinter import *
+from tkinter import ttk
+
+a = int(input("please enter a number:"))
+
+try:
+    root1 = Tk()
+    root1.title('this is a program')
+    ttk.Button(root1, text = "Hello World").grid()
+    ttk.Button(root1, text = "I'm happy ").grid()
+    ttk.Label(root1, text = 'hi my name is glory').grid()
+    ttk.Checkbutton(root1, text = 'kawawonga').grid()
+except :
+    pass
+
+print('hello')
+root1.mainloop()
 
 
