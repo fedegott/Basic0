c419eadba82782cbd51de83880ee65d5329abe69
federicogottardo <federicogottard@gmail.com>
2018-03-18 06:37:00 +0000

    change

---
+++ Basic1.py
@@ -0,0 +1,15 @@
+
+#iterate through characters of a collections of string
+
+wordlist = ['cat','dog','rabbit']
+letterlist = [ ]
+for aword in wordlist:
+    for aletter in aword:
+        letterlist.append(aletter)
+print(letterlist)
+
+word = 'jose'
+a = []
+for w in word:
+    a.append(w)
+print(a)
